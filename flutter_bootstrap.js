fa133656e07219c4ff78c1e347c04a9d941c4618
park chang <allenjoyday@gmail.com>
2026-04-01 16:41:24 +0900

    detail, capture

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "886085804"
+    serviceWorkerVersion: "2608708761"
   }
 });
